3e399d85f4cb0f6dfc1ce6cc8421456363591fbe
muhammadfahadamir <amirkhankahout@gmail.com>
2025-05-18 19:40:48 +0500

    the use of funtion for addition

--- tutorial_01.py
+++ tutorial_01.py
@@ -3,4 +3,12 @@ import pandas as pd
 print("this is first program")
 x = 1
 y = 2
-print(x + y)
+#print(x + y)
+def add(x_in,y_in):
+    return  x_in + y_in
+
+print("this is the addition of x and y =",add(x,y))
+print("sum of 3 and 4 is =",add(3,4))
+print("the sum of 11 and 7 is :",add(7,11))
+print("the sum of 9 and 2 is :",add(9,2))
+print("the sum of 6 and 7 is :",add(6,7))
